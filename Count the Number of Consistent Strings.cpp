1dd1a350bb63732d81c4a088e3a8f4b1e6c4ac0d
Indominous1 <93007035+Indominous1@users.noreply.github.com>
2022-09-08 01:42:57 +0530

    Create Count the Number of Consistent Strings.cpp

---
+++ Count the Number of Consistent Strings.cpp
@@ -0,0 +1,69 @@
+Problem Statement-
+You are given a string allowed consisting of distinct characters and an array of strings words. A string is consistent if all characters in the string appear in the 
+string allowed. Return the number of consistent strings in the array words.
+  
+Example 1:
+Input: allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
+
+Output: 2
+
+Explanation: Strings "aaab" and "baa" are consistent since they only contain characters 'a' and 'b'.
+
+Example 2:
+Input: allowed = "abc", words = ["a","b","c","ab","ac","bc","abc"]
+
+Output: 7
+
+Explanation: All strings are consistent.
+
+Example 3:
+Input: allowed = "cad", words = ["cc","acd","b","ba","bac","bad","ac","d"]
+
+Output: 4
+
+Explanation: Strings "cc", "acd", "ac", and "d" are consistent.
+ 
+Constraints:
+-> 1 <= words.length <= 104
+-> 1 <= allowed.length <= 26
+-> 1 <= words[i].length <= 10
+-> The characters in allowed are distinct.
+words[i] and allowed contain only lowercase English letters.
+  
+ Related Topics-
+> Array 
+> Hash Table
+> String
+>Bit Manipulation
+
+   -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Solution-
+class Solution {
+public:
+    int countConsistentStrings(string al, vector<string>& v) {
+       map<char,int> m;
+       int co=0;
+       bool st=true;
+       for(int i=0;i<al.size();i++)
+       {
+           m[al[i]]++;
+       }
+       for(int i=0;i<v.size();i++)
+       {
+           st=true;
+           string x=v[i];
+           for(int j=0;j<x.size();j++)
+           {
+               if(m.find(x[j])==m.end())
+               {
+                   st=false;
+                   break;
+               }
+           }
+           if(st==true)
+               co++;
+       }
+        return co;
+    }
+};
+
