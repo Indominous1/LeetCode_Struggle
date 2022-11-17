9a59bf5e89800359e564694c30634f7cd4307787
Indominous1 <93007035+Indominous1@users.noreply.github.com>
2022-11-17 21:20:11 +0530

    Create Sum of Unique Elements.cpp

---
+++ Sum of Unique Elements.cpp
@@ -0,0 +1,58 @@
+Problem Statement-
+You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
+Return the sum of all the unique elements of nums.
+
+Example 1:
+Input: nums = [1,2,3,2]
+
+Output: 4
+
+Explanation:
+> The unique elements are [1,3], and the sum is 4.
+
+Example 2:
+Input: nums = [1,1,1,1,1]
+
+Output: 0
+
+Explanation:
+> There are no unique elements, and the sum is 0.
+
+Example 3:
+Input: nums = [1,2,3,4,5]
+
+Output: 15
+
+Explanation:
+> The unique elements are [1,2,3,4,5], and the sum is 15.
+
+Constraints:
+-> 1 <= nums.length <= 100
+-> 1 <= nums[i] <= 100
+  
+Related Topics-
+> Array
+> Hash Table
+> Counting
+  
+   ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Solution-
+class Solution {
+public:
+    int sumOfUnique(vector<int>& n) {
+        map<int,int>mp;
+        int sum=0;
+        for(int i=0;i<n.size();i++)
+        {
+            mp[n[i]]++;
+        }
+        for(auto itr=mp.begin();itr!=mp.end();itr++)
+        {
+            if(itr->second ==1)
+            {
+                sum+=itr->first;
+            }
+        }
+    return sum;}
+};
+
