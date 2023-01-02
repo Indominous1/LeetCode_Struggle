fccfe0929a427c7747ce35cdb1853e2e30e8a084
Indominous <93007035+Indominous1@users.noreply.github.com>
2023-01-02 21:09:47 +0530

    Create Middle of the Linked List.cpp

---
+++ Middle of the Linked List.cpp
@@ -0,0 +1,51 @@
+Problem Statement-
+Given the head of a singly linked list, return the middle node of the linked list.
+If there are two middle nodes, return the second middle node.
+
+Example 1:
+Input: head = [1,2,3,4,5]
+
+Output: [3,4,5]
+
+Explanation:
+> The middle node of the list is node 3.
+
+Example 2:
+Input: head = [1,2,3,4,5,6]
+
+Output: [4,5,6]
+
+Explanation:
+> Since the list has two middle nodes with values 3 and 4, we return the second one.
+ 
+Constraints:
+-> The number of nodes in the list is in the range [1, 100].
+-> 1 <= Node.val <= 100
+  
+Related Topics-
+> Linked List
+> Two Pointers
+  
+   ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Solution-
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *temp=head;
+        int n=0;
+        while(temp)
+        {
+            n++;
+            temp=temp->next;
+        }
+        int j=0;
+        while(j!=n/2)
+        {
+            head=head->next;
+            j++;
+        }
+        ListNode *g=head;
+        return g;
+    }
+};
+
