78acd09ca3566ce4b27335e119dd1cf5f17e6b4c
Indominous <93007035+Indominous1@users.noreply.github.com>
2023-01-16 20:23:00 +0530

    Create Insert Interval.py

---
+++ Insert Interval.py
@@ -0,0 +1,63 @@
+Problem Statement-
+You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals
+is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals
+(merge overlapping intervals if necessary).
+
+Return intervals after the insertion.
+
+Example 1:
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+
+Output: [[1,5],[6,9]]
+
+Example 2:
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+
+Output: [[1,2],[3,10],[12,16]]
+
+Explanation:
+> Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+
+Constraints:
+-> 0 <= intervals.length <= 104
+-> intervals[i].length == 2
+-> 0 <= starti <= endi <= 105
+-> intervals is sorted by starti in ascending order.
+-> newInterval.length == 2
+-> 0 <= start <= end <= 105
+  
+Related Topics-
+> Array
+  
+   ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Solution-
+class Solution(object):
+     def insert(self, intervals, newInterval):
+        
+        # Constant to help us access start point and end point of interval
+        START, END = 0, 1
+        
+        s, e = newInterval[START], newInterval[END]
+        
+        left, right = [], []
+        
+        for cur_interval in intervals:
+            
+            if cur_interval[END] < s:
+                # current interval is on the left-hand side of newInterval
+                left += [ cur_interval ]
+                
+            elif cur_interval[START] > e:
+                # current interval is on the right-hand side of newInterval
+                right += [ cur_interval ]
+                
+            else:
+                # current interval has overlap with newInterval
+                # merge and update boundary
+                s = min(s, cur_interval[START])
+                e = max(e, cur_interval[END])
+                
+        return left + [ [s, e] ] + right 
+      
