7fd7b54c2c2d1baf83f21262687490d7dd55538c
Indominous <93007035+Indominous1@users.noreply.github.com>
2023-02-09 20:36:22 +0530

    Create  Reverse Substrings Between Each Pair of Parentheses.cpp

---
+++ Reverse Substrings Between Each Pair of Parentheses.cpp
@@ -0,0 +1,62 @@
+Problem Statement-
+You are given a string s that consists of lower case English letters and brackets.
+Reverse the strings in each pair of matching parentheses, starting from the innermost one.
+
+Your result should not contain any brackets.
+
+Example 1:
+Input: s = "(abcd)"
+
+Output: "dcba"
+
+Example 2:
+Input: s = "(u(love)i)"
+
+Output: "iloveu"
+
+Explanation:
+> The substring "love" is reversed first, then the whole string is reversed.
+
+Example 3:
+Input: s = "(ed(et(oc))el)"
+
+Output: "leetcode"
+
+Explanation:
+> First, we reverse the substring "oc", then "etco", and finally, the whole string.
+ 
+Constraints:
+-> 1 <= s.length <= 2000
+-> s only contains lower case English characters and parentheses.
+-> It is guaranteed that all parentheses are balanced.
+  
+Related Topics-
+> String
+> Stack
+  
+   ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Solution-
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        vector<string> v;
+        string ans="";
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(')
+            {
+                v.push_back(ans);
+                ans="";
+            }
+            else if(s[i]==')')
+            {
+                reverse(ans.begin(),ans.end());
+                ans=v.back()+ans;
+                v.pop_back();
+            }
+            else
+                ans+=s[i];
+        }
+   return ans; }
+};
+
