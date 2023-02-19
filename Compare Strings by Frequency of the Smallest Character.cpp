4e8c99e5f1b1b946e2ed4cce0ca2244ced7eacd4
Indominous <93007035+Indominous1@users.noreply.github.com>
2023-02-19 20:46:37 +0530

    Create Compare Strings by Frequency of the Smallest Character.cpp

---
+++ Compare Strings by Frequency of the Smallest Character.cpp
@@ -0,0 +1,77 @@
+Problem Statement-
+Let the function f(s) be the frequency of the lexicographically smallest character in a non-empty string s. For example, if s = "dcce" then f(s) = 2 because the lexicographically
+smallest character is 'c', which has a frequency of 2.
+
+You are given an array of strings words and another array of query strings queries. For each query queries[i], count the number of words in words such that 
+f(queries[i]) < f(W) for each W in words.
+
+Return an integer array answer, where each answer[i] is the answer to the ith query.
+
+Example 1:
+Input: queries = ["cbd"], words = ["zaaaz"]
+
+Output: [1]
+
+Explanation: 
+> On the first query we have f("cbd") = 1, f("zaaaz") = 3 so f("cbd") < f("zaaaz").
+
+Example 2:
+Input: queries = ["bbb","cc"], words = ["a","aa","aaa","aaaa"]
+
+Output: [1,2]
+
+Explanation:
+> On the first query only f("bbb") < f("aaaa"). On the second query both f("aaa") and f("aaaa") are both > f("cc").
+ 
+Constraints:
+-> 1 <= queries.length <= 2000
+-> 1 <= words.length <= 2000
+-> 1 <= queries[i].length, words[i].length <= 10
+-> queries[i][j], words[i][j] consist of lowercase English letters.
+  
+Related Topics-
+> Array
+> Hash Table
+> String
+> Binary Search
+> Sorting
+  
+   ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Solution-
+class Solution {
+public:
+    int countd(char c,string s)
+    {
+        int cnt=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]==c)
+            cnt++;
+        }
+        return cnt;
+    }
+    vector<int> numSmallerByFrequency(vector<string>& qu, vector<string>& w) {
+        map<int,int> s;
+        vector<int> v;
+        for(int i=0;i<w.size();i++)
+        {
+            sort(w[i].begin(),w[i].end());
+            int y= countd(w[i][0],w[i]);
+            s[y]++;
+        }
+        for(int i=0;i<qu.size();i++)
+        {
+            sort(qu[i].begin(),qu[i].end());
+            int d = countd(qu[i][0],qu[i]),cnt=0;
+            cout<<qu[i]<<" "<<d<<endl;
+            for(auto itr=s.begin();itr!=s.end();itr++)
+            {
+                if(itr->first > d)
+                    cnt+=itr->second;
+            }
+            v.push_back(cnt);
+        }
+        return v;
+    }
+};
+
